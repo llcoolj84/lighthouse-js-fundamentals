21b1ce8b3c1a5a8b599fb2db46257a42d2190df3
llcoolj84 <llcj84@gmail.com>
2017-08-10 23:48:56 +0000

    adds conditionals exercises

---
+++ ageCalgulator.js
@@ -0,0 +1,9 @@
+function ageCalculator("name",yearOfBirth,currentYear) {
+
+  console.log(name + " is " , currentYear - yearOfBirth + "s old.");
+
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
